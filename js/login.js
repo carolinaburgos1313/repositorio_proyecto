69e8e02e17ac087f6ba40b65ec177dbf58c18aeb
carolinaburgos1313 <ana.burgos.1313@hotmail.com>
2020-08-11 22:52:36 -0300

    hola

---
+++ js/login.js
@@ -0,0 +1,20 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+
+});
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+
+});
+
+function redirection(){ 
+   
+   
+    window.location.replace("login.html");
+   
+    } 
+    
